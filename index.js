26d91b14ce006db1432e0902fb772ba7f3fbc8a4
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-08-30 12:07:39 -0300

    Adiciona a conexão com o banco assim como suas operações

--- index.js
+++ index.js
@@ -1,71 +1,100 @@
 const express = require('express');
-const app = express();
+const { MongoClient, ObjectId } = require('mongodb');
 
-app.use(express.json());
+const url = 'mongodb://root:root@localhost:27017/admin';
+const dbName = 'jornada-backend'
+const client = new MongoClient(url);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const main = async () => {
+  console.log("Conectando ao banco de dados...");
+  await client.connect();
+  console.log("Banco de dados conectado com sucesso!");
 
-app.get('/oi', function (req, res) {
-  res.send('Olá mundo!')
-})
+  const db = client.db(dbName);
+  const collection = db.collection('heroes');
 
-const heroes = [
-  { id: '1', nome: 'Superman' },
-  { id: '2', nome: 'Batman' },
-  { id: '3', nome: 'Mulher Maravilha' },
-  { id: '4', nome: 'Homem-Aranha' },
-  { id: '5', nome: 'Homem de Ferro' },
-  { id: '6', nome: 'Thor' },
-  { id: '7', nome: 'Capitão América' },
-  { id: '8', nome: 'Hulk' },
-  { id: '9', nome: 'Flash' },
-  { id: '10', nome: 'Lanterna Verde' },
-  { id: '11', nome: 'Viúva Negra' },
-  { id: '12', nome: 'Pantera Negra' },
-  { id: '13', nome: 'Aquaman' },
-  { id: '14', nome: 'Wolverine' },
-  { id: '15', nome: 'Mulher-Gato' },
-  { id: '16', nome: 'Deadpool' },
-  { id: '17', nome: 'Doutor Estranho' },
-  { id: '18', nome: 'Capitã Marvel' },
-  { id: '19', nome: 'Homem-Formiga' },
-  { id: '20', nome: 'Viúva Negra' }
-];
+  const app = express();
 
-app.get('/heroes', (req, res) => res.send(heroes));
+  app.use(express.json());
 
-app.post('/heroes', (req, res) => { 
-  const hero = req.body;
-  heroes.push(hero);
-  
-  res.send('Registro criado com sucesso!') 
-});
+  app.get('/', function (req, res) {
+    res.send('Hello World!')
+  })
 
-app.get('/heroes/:id', (req, res) => {
-  const id = req.params.id;
-  const hero = heroes.find((hero) => hero.id === id);
+  app.get('/oi', function (req, res) {
+    res.send('Olá mundo!')
+  })
 
-  res.send(hero);
-});
+  const heroes = [
+    { id: '1', nome: 'Superman' },
+    { id: '2', nome: 'Batman' },
+    { id: '3', nome: 'Mulher Maravilha' },
+    { id: '4', nome: 'Homem-Aranha' },
+    { id: '5', nome: 'Homem de Ferro' },
+    { id: '6', nome: 'Thor' },
+    { id: '7', nome: 'Capitão América' },
+    { id: '8', nome: 'Hulk' },
+    { id: '9', nome: 'Flash' },
+    { id: '10', nome: 'Lanterna Verde' },
+    { id: '11', nome: 'Viúva Negra' },
+    { id: '12', nome: 'Pantera Negra' },
+    { id: '13', nome: 'Aquaman' },
+    { id: '14', nome: 'Wolverine' },
+    { id: '15', nome: 'Mulher-Gato' },
+    { id: '16', nome: 'Deadpool' },
+    { id: '17', nome: 'Doutor Estranho' },
+    { id: '18', nome: 'Capitã Marvel' },
+    { id: '19', nome: 'Homem-Formiga' },
+    { id: '20', nome: 'Viúva Negra' }
+  ];
 
-app.put('/heroes/:id', (req, res) => {
-  const id  = req.params.id;
-  const hero = req.body.nome;
+  app.get('/heroes', async (req, res) => {
+    const itens = await collection.find().toArray();
+    console.log(itens);
+    res.send(itens);
+  });
 
-  const indexHero = heroes.findIndex((hero) => hero.id === id);
-  heroes[indexHero].nome = hero;
+  app.post('/heroes', async (req, res) => { 
+    const item = req.body;
+    
+    await collection.insertOne(item);
 
-  res.send('Item atualizado com sucesso');
-});
+    res.send(item);
+  });
 
-app.delete('/heroes/:id', (req, res) => {
-  const id = req.params.id;
-  indexHero = heroes.findIndex((hero) => hero.id === id);
-  heroes.splice(indexItem, 1);
-  
-  res.send('Item excluído com sucesso');
-});
+  app.get('/heroes/:id', async (req, res) => {
+    const id = req.params.id;
+    const item = await collection.findOne({
+      _id: new ObjectId(id)
+    });
 
-app.listen(3000)
+    res.send(item);
+  });
+
+  app.put('/heroes/:id', async (req, res) => {
+    const id  = req.params.id;
+    const item = req.body.nome;
+
+    await collection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: item }
+    )
+
+    res.send(item);
+  });
+
+  app.delete('/heroes/:id', (req, res) => {
+    const id = req.params.id;
+    
+    collection.deleteOne(
+      { _id: new ObjectId(id) },
+      { $set: item }
+    );
+    
+    res.send('Item excluído com sucesso');
+  });
+
+  app.listen(3000);
+}
+
+main();
